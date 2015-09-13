c75e8fa343973e131cf22eff5a51de36d55dd872
Fabian Zimmer <fabian@qfma.de>
2015-09-13 14:03:18 +0100

    Added the page model to the model controller

--- src/controllers/page.go
+++ src/controllers/page.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/russross/blackfriday"
+
+	"github.com/fuzzyants/wikingo/src/models"
 )
 
 const dataFile = "../data/index.md"
@@ -22,19 +24,24 @@ func NewPageController() *PageController {
 	return &PageController{}
 }
 
-// GetUser retrieves an individual user resource
+// GetPage parses a Markdown file and converts it into HTML
 func (pc PageController) GetPage(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	//print requested path to console
 	fmt.Println("path: ", r.URL.Path)
 
-	//load a file
+	// Read the datafile
 	dat, err := ioutil.ReadFile(dataFile)
 	if err != nil {
 		log.Fatal("ReadFile"+": ", err)
 	}
 
-	output := blackfriday.MarkdownCommon(dat)
+	// Instantiate a new Page
+	p := models.Page{
+		Content: dat,
+	}
+
+	output := blackfriday.MarkdownCommon(p.Content)
 	//send it to the client
 	fmt.Fprintf(w, string(output))
 }
