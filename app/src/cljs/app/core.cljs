57e7d4b9dc1aabde9d50a9069893e833df11eab8
Fabian Zimmer <fabian@qfma.de>
2015-09-22 21:19:43 +0100

    Refactoring and comments

--- app/src/cljs/app/core.cljs
+++ app/src/cljs/app/core.cljs
@@ -11,40 +11,51 @@
 ;; -------------------------
 ;; Views
 
+; A reagent atom that can be updated
 (def page (atom nil))
 
+; Takes the json response from the wikingo server and stores the content in
+; the page atom.
 (defn page-handler [response]
   (reset! page (get (js->clj response) "content")))
 
+; A reagent component that renders a page using the wikingo json api
+; let binds the page content and will only be executed *once*
 (defn render-page []
   (let [get-page (fn [] (GET "http://localhost:9090/page" {:handler page-handler}))]
+  ; Get the page content
   (get-page)
+  ; Rendering function that will update every time the @page atom changes
   (fn []
-    [:div {:dangerouslySetInnerHTML
+    [:div {:dangerouslySetInnerHTML ; Only a good idea if we trust the markdown
               {:__html (md/md->html @page)}}])))
 
+; Update the page atom
 (defn atom-input [value]
   [:textarea {:rows 5
            :cols 50
            :value @value
            :on-change #(reset! value (-> % .-target .-value))}])
 
-(defn shared-state []
+; Component to update the markdown content
+(defn update-page []
   [:div
    [:p "The raw markdown is now:"]
    [:p @page]
    [:p "Change it here: "]
    [:p [atom-input page]]])
 
+; Component that renders the homepage.
 (defn home-page []
   [:div [render-page]
-   [:div [shared-state]]
+   [:div [update-page]]
    [:div [:a {:href "#/about"} "go to about page"]]
    ])
 
 (defn current-page []
   [:div [(session/get :current-page)]])
 
+; Test handler
 (defn handler [response]
   (.log js/console (str response)))
 
